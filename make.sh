83e4aa36f49883b2b0e0ca0d1ee9a0111cc19e8f
Panchajanya1999 <rsk52959@gmail.com>
2018-07-03 15:43:23 +0530

    Lets modify according Semaphore
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- make.sh
+++ make.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 make O=out ARCH=arm64 X00TD_defconfig
-#ccache -M 300G
+ccache -M 300G
 BUILD_START=$(date +"%s")
 
 blue='\033[0;34m' cyan='\033[0;36m'
@@ -15,6 +15,13 @@ export CLANG_TRIPLE=aarch64-linux-gnu-
 export CROSS_COMPILE=$KERNEL_DIR/aarch64-linux-android-4.9/bin/aarch64-linux-android-
 
 make -j8 O=out 
+BUILD_END=$(date +"%s")
+DIFF=$(($BUILD_END - $BUILD_START))
+blue='\033[0;34m' cyan='\033[0;36m'
+yellow='\033[0;33m'
+red='\033[0;31m'
+nocol='\033[0m'
+echo -e "$yellow Build completed in $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) seconds.$nocol";
                       
                                            
                       
