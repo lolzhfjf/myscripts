2fc3166be01ac2bd3acc5c6b58aeb0df2625ef3a
Panchajanya1999 <rsk52959@gmail.com>
2018-07-14 14:14:28 +0530

    zip.sh: Added TG Channel Pushing
    Signed-off-by: Panchajanya1999 <rsk52959@gmail.com>

--- zip.sh
+++ zip.sh
@@ -3,5 +3,14 @@ cd AnyKernel2
 mv /home/runner/Azure-ASUS/out/arch/arm64/boot/Image.gz-dtb Image.gz-dtb
 BUILD_TIME=$(date +"%Y%m%d-%T")
 zip -r AzurE-Oreo-X00TD-$BUILD_TIME *
-curl -ftp-pasv -T AzurE-Oreo-X00TD-$BUILD_TIME.zip ftp://AzurE:GiC5Ht0Ge0ez@uploads.androidfilehost.com
-#curl -F chat_id="-1232545787" -F document=AzurE-Oreo-X00TD-$BUILD_TIME.zip https://api.telegram.org/bot508917321:AAFcSMMFzWdr-2wBsmKFhypu2eS7MC0THx0/sendDocument
+
+
+#POSTCHANNEL=$(printf 'Build Completed Successfully..!!\nDevice : ASUS X00TD\nBuild Details : $(BUILD_TIME)\n\nInstructions To Flash This Build :- \n 1)Dirty Flashing Your ROM is Necessary...!!!\n 2)Wipe Dalvik & Cache after flashing...!!\n\nNote :- This Build is a Continuous Integration Build, Compiled with CLANG 5...!!!\nEnjoy Flashing\nThank You -@Whiteliners')
+
+
+#curl -ftp-pasv -T AzurE-Oreo-X00TD-$BUILD_TIME.zip ftp://AzurE:GiC5Ht0Ge0ez@uploads.androidfilehost.com
+
+curl -s -X POST "https://api.telegram.org/bot508917321:AAFcpu2eS7MC0THx0/sendMessage" -d chat_id="-1001232545787" -d text="Build Completed for ASUS X00TD"
+
+curl -F chat_id="-1001232545787" -F document=AzurE-Oreo-X00TD-$BUILD_TIME.zip https://api.telegram.org/bot508917321:AAFcSMMFzWdr-2wBsmKFhypu2eS7MC0THx0/sendDocument
+
